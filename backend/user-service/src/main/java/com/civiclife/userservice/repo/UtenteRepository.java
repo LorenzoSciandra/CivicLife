77fc4511a6a3b0b20ade51da88431af37f269fd2
Lorenzo Sciandra <49808768+LorenzoSciandra@users.noreply.github.com>
2022-12-07 11:25:12 +0100

    Delete backend/user-service directory

--- backend/user-service/src/main/java/com/civiclife/userservice/repo/UtenteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.civiclife.userservice.repo;
-
-import com.civiclife.userservice.model.Utente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UtenteRepository extends CrudRepository<Utente, Long> {
-
-}
