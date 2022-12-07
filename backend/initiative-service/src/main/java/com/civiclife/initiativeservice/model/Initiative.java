ecc7337dc38c39cc9401eaf38a1576d725ed9181
Lorenzo Sciandra <49808768+LorenzoSciandra@users.noreply.github.com>
2022-12-07 16:11:13 +0100

    Update Initiative.java

--- backend/initiative-service/src/main/java/com/civiclife/initiativeservice/model/Initiative.java
+++ backend/initiative-service/src/main/java/com/civiclife/initiativeservice/model/Initiative.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.sql.Timestamp;
 import java.util.List;
 
-@Document(collection = "initiatives")
+@Document(collection = "Initiative")
 public class Initiative {
 
     @Id
