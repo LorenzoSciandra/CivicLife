24165df1ed64565164c4c334b77e48715cec5f4b
lorenzosciandra <lorenzo.sciandra.business@gmail.com>
2022-12-10 10:07:39 +0100

    dockerizzato

--- backend/initiative-service/src/main/java/com/civiclife/initiativeservice/model/Initiative.java
+++ backend/initiative-service/src/main/java/com/civiclife/initiativeservice/model/Initiative.java
@@ -1,12 +1,17 @@
 package com.civiclife.initiativeservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.sql.Timestamp;
 import java.util.List;
 
-@Document(collection = "Initiative")
+@Data
+@AllArgsConstructor
+@Document(collection = "initiatives")
 public class Initiative {
 
     @Id
