bbcbd156568f133e97df11c2cb998364231456a0
lorenzosciandra <lorenzo.sciandra.business@gmail.com>
2022-12-12 15:40:54 +0100

    revert per errore git

---
+++ backend/initiative-service/src/main/java/com/civiclife/initiativeservice/InitiativeServiceApplication.java
@@ -0,0 +1,19 @@
+package com.civiclife.initiativeservice;
+
+import com.civiclife.initiativeservice.repo.InitiativeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableMongoRepositories
+public class InitiativeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitiativeServiceApplication.class, args);
+        System.out.println("PROVA");
+    }
+}
